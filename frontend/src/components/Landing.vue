7852b47883600d2b922e77e10026d8685d336c4b
XuanGiaHanNguyen <han316@usf.edu>
2025-05-19 17:05:02 +0700

    updated landing pages

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-  <div class="h-[75vh] flex justify-center items-center">
-   
+  <div class="h-[72vh] flex flex-col justify-center items-center gap-2">
+   <h1 class="font-bold text-9xl text-gray-500">Algorythms</h1>
+   <div class="text-xl font-semibold px-14 py-1 rounded-lg text-gray-600">
+        <p>Visualizing Data Structure and Algorithms</p>
+   </div>
+   <div class="flex flex-row gap-4">
+        <button class="px-8 py-2 text-lg font-medium rounded-lg bg-gray-300 text-gray-600">
+            Learn More
+        </button>
+        <button class="px-8 py-2 text-lg font-medium rounded-lg bg-gray-300 text-gray-600">
+            Guide
+        </button>
+   </div>
   </div>
 </template>
