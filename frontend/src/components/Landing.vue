51f97c1ef498e883e6187ec9d2fd51ef76e82fbc
XuanGiaHanNguyen <han316@usf.edu>
2025-05-19 14:57:20 +0700

    fixed

--- frontend/src/components/Landing.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-  import { ref } from 'vue';
-</script>
-
-<template>
-  <h1 class="text-lg bg-blue-200">Landing</h1>
-</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Sort from "@/components/Sort.vue";
 import Path from "@/components/Path.vue";
-import Landing from "@/components/Landing.vue"
 
 const routes = [
-    {path: '/sort', name: 'Sort', component: Sort},
+    {path: '/', name: 'Sort', component: Sort},
     {path: '/path', name: 'Path', component: Path},
-    {path: '/', name: 'Landing', component: Landing},
 ];
 
 const router = createRouter({
