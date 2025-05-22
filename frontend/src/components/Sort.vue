d113e56f79b98a251a6ccf38ba5ddd15befebe8f
XuanGiaHanNguyen <han316@usf.edu>
2025-05-22 08:02:17 +0700

    changed to cleaner design

--- frontend/src/components/Sort.vue
+++ frontend/src/components/Sort.vue
@@ -3,19 +3,7 @@
 </script>
 
 <template>
-  <div class=" flex flex-row px-4">
-    <!-- Left side with vertical "SORTING" text -->
-    <div class="w-1/12 flex items-center justify-center">
-      <div class="transform -rotate-90 text-8xl font-bold text-gray-500 tracking-wide">
-        SORTING
-      </div>
-    </div>
-    
-    <!-- Right side with white background box -->
-    <div class="w-11/12 p-4">
-      <div class="bg-white rounded-lg h-[85vh]">
-        <!-- Content for the white box goes here -->
-      </div>
-    </div>
+  <div class="bg-white h-[83vh] mt-4 mx-10 rounded-lg">
+
   </div>
 </template>
